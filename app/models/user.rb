f2e40d6ff6dc2c7c496247e72e785e59bd34c804
Michael Nipper <mjn4406@gmail.com>
2013-02-20 19:05:35 -0500

    Add sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ActiveRecord::Base
   validates :name, length: { maximum: 50 }, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
                 uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,4 +1,25 @@
 require 'spec_helper'
 
+=begin
 describe "UserPages" do
+  subject { page }
+  describe "profile page" do
+    pending "until i fix capybara"
+
+    let(:user) { FactoryGirl.create(:user)  }
+    #before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+  end
+
+  describe "signup" do
+    pending "until i fix capybara"
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      #expect { click_button "Creat my account" }.not_to change(User, :count)
+    end
+  end
 end
+=end
